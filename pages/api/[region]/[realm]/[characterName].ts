fc6e9594f60f35666e4961c92fe2348fbc84090e
Markus Kullberg <kullberg.markus@gmail.com>
2021-01-01 23:39:51 +0200

    WIP

--- pages/api/[region]/[realm]/[characterName].ts
+++ pages/api/[region]/[realm]/[characterName].ts
@@ -1,24 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { region, realm, characterName } = req.query;
-    console.info(
-        `Fetching character info for ${characterName}-${realm}-${region}`
-    );
-    const data = await fetchCharacter(
-        query as unknown as CharacterQuery
-    );
-
-    if (data) {
-        const arenaStatistics = reduceArenaStatistics(data);
-        const media = reduceMedia(data.media);
-
-        props = {
-            props: {
-                arena: arenaStatistics,
-                media,
-            },
-        };
-    }
-
+    // TODO
 }
